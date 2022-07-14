818a859166b2490e0ee4d5ee8da20ac8f1bdc59e
smalay <smalay@slb.com>
2022-07-14 11:50:01 +0530

    Adding initial version

--- cache.go
+++ cache.go
@@ -3,21 +3,35 @@ package cache
 import "time"
 
 type Cache struct {
+	Key, Value string
+	KVPair     map[string]string
 }
 
-func NewCache() Cache {
-	return Cache{}
+func NewCache(k string, v string) Cache {
+	kvp := make(map[string]string)
+	return Cache{Key: k, Value: v, KVPair: kvp}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (obj Cache) Get(key string) (string, bool) {
+	val, ok := obj.KVPair[key]
+	if ok == true {
+		return val, true
+	} else {
+		return "", false
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (obj Cache) Put(key, value string) {
+	obj.KVPair[key] = value
 }
 
-func (receiver) Keys() []string {
+func (obj Cache) Keys() []string {
+	cacheKeys := make([]string, len(obj.KVPair))
+	for k := range obj.KVPair {
+		cacheKeys = append(cacheKeys, k)
+	}
+	return cacheKeys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (obj Cache) PutTill(key, value string, deadline time.Time) {
 }
